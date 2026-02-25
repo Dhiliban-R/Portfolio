58c3722eabb24fef171683c802674d268c6d5efd
Dhiliban Raja <dhilipanrc@gmail.com>
2026-02-26 02:05:29 +0530

    chore: setup github actions for pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Portfolio/', // Needed for GitHub Pages to resolve assets correctly
 })
